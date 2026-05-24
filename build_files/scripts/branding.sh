7768847993084c5ee26b223ab60f5264469af5a6
mrtrick37 <261214137+mrtrick37@users.noreply.github.com>
2026-05-23 21:36:07 -0400

    f

--- build_files/scripts/branding.sh
+++ build_files/scripts/branding.sh
@@ -70,6 +70,7 @@ ID=fedora
 VERSION="44"
 VERSION_ID="44"
 ANSI_COLOR="0;34"
+LOGO=kyth
 HOME_URL="https://github.com/mrtrick37/kyth"
 SUPPORT_URL="https://github.com/mrtrick37/kyth/discussions"
 BUG_REPORT_URL="https://github.com/mrtrick37/kyth/issues"
